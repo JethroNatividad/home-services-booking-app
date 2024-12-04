86fbb4d422d3118ee7d0784a0c77f96ea14b7d66
JethroNatividad <natividad.jet@gmail.com>
2024-12-05 01:07:08 +0800

    feat: create models

---
+++ app/Models/Service.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Service extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'name', 'description', 'price', 'images', 'category_id'];
+
+    protected $casts = [
+        'images' => 'array',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,12 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
+        'completed',
+        'contact_number',
+        'address',
+        'lat',
+        'long'
     ];
 
     /**

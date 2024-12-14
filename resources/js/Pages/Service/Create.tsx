8f84d23d0d57c250b9b80f6fcfbdef0813fc1ff4
JethroNatividad <natividad.jet@gmail.com>
2024-12-15 03:12:02 +0800

    feat: create back button

--- resources/js/Pages/Service/Create.tsx
+++ resources/js/Pages/Service/Create.tsx
@@ -1,3 +1,4 @@
+import BackButton from "@/components/back-button";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
@@ -10,6 +11,7 @@ import {
 import Layout from "@/layouts/Layout";
 import { Category } from "@/types";
 import { useForm } from "@inertiajs/react";
+import { ChevronLeft } from "lucide-react";
 import React from "react";
 
 type Props = {
@@ -32,64 +34,71 @@ const Create = ({ categories }: Props) => {
 
     return (
         <Layout>
-            <h1 className="text-xl">Create Service</h1>
-            <form onSubmit={handleSubmit} className="w-full">
-                <div className="space-y-4">
-                    <div className="w-full space-y-2">
-                        <label htmlFor="role" className="block text-sm">
-                            Category
-                        </label>
-                        <Select
-                            onValueChange={(value) => {
-                                setData("category_id", value);
-                            }}
-                            value={data.category_id}
-                        >
-                            <SelectTrigger>
-                                <SelectValue placeholder="Select" />
-                            </SelectTrigger>
-                            <SelectContent>
-                                {categories.map((category) => (
-                                    <SelectItem
-                                        key={category.id}
-                                        value={category.id.toString()}
-                                    >
-                                        {category.name}
-                                    </SelectItem>
-                                ))}
-                            </SelectContent>
-                        </Select>
-                        {errors.category_id && (
-                            <div className="text-red-500 text-sm">
-                                {errors.category_id}
-                            </div>
-                        )}
-                    </div>
+            <div className="flex items-center space-x-4">
+                <BackButton />
+                <h1 className="text-xl">Create Service</h1>
+            </div>
+            <div className="container mx-auto p-4">
+                <form onSubmit={handleSubmit} className="w-full">
+                    <div className="space-y-4">
+                        <div className="w-full space-y-2">
+                            <label htmlFor="role" className="block text-sm">
+                                Category
+                            </label>
+                            <Select
+                                onValueChange={(value) => {
+                                    setData("category_id", value);
+                                }}
+                                value={data.category_id}
+                            >
+                                <SelectTrigger>
+                                    <SelectValue placeholder="Select" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    {categories.map((category) => (
+                                        <SelectItem
+                                            key={category.id}
+                                            value={category.id.toString()}
+                                        >
+                                            {category.name}
+                                        </SelectItem>
+                                    ))}
+                                </SelectContent>
+                            </Select>
+                            {errors.category_id && (
+                                <div className="text-red-500 text-sm">
+                                    {errors.category_id}
+                                </div>
+                            )}
+                        </div>
 
-                    <div className="w-full space-y-2">
-                        <label htmlFor="name" className="block text-sm">
-                            Name
-                        </label>
-                        <Input
-                            id="name"
-                            type="text"
-                            value={data.name}
-                            onChange={(e) => setData("name", e.target.value)}
-                            placeholder="Juan"
-                            className="w-full"
-                        />
-                        {errors.name && (
-                            <div className="text-red-500 text-sm">
-                                {errors.name}
-                            </div>
-                        )}
-                    </div>
+                        <div className="w-full space-y-2">
+                            <label htmlFor="name" className="block text-sm">
+                                Name
+                            </label>
+                            <Input
+                                id="name"
+                                type="text"
+                                value={data.name}
+                                onChange={(e) =>
+                                    setData("name", e.target.value)
+                                }
+                                placeholder="Juan"
+                                className="w-full"
+                            />
+                            {errors.name && (
+                                <div className="text-red-500 text-sm">
+                                    {errors.name}
+                                </div>
+                            )}
+                        </div>
 
-                    <Button type="submit" disabled={processing}>
-                        Create Service
-                    </Button>
-                </div>
-            </form>
+                        <Button type="submit" disabled={processing}>
+                            Create Service
+                        </Button>
+                    </div>
+                </form>
+            </div>
         </Layout>
     );
 };

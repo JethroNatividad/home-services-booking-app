af399761b8494e12b8917651a153e885f0c0c63b
JethroNatividad <natividad.jet@gmail.com>
2025-01-05 00:39:22 +0800

    feat: implement pagination

--- resources/js/Pages/ServiceProvider/columns.tsx
+++ resources/js/Pages/ServiceProvider/columns.tsx
@@ -175,7 +175,19 @@ const UserCell = ({ row }: { row: Row<Booking> }) => {
 export const columns: ColumnDef<Booking>[] = [
     {
         accessorKey: "id",
-        header: "ID",
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() =>
+                        column.toggleSorting(column.getIsSorted() === "asc")
+                    }
+                >
+                    ID
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            );
+        },
     },
     {
         accessorKey: "created_at",
